ce724856e665bb1ca6b52bc6633cc220e35943c6
belenespilman <belen.espilman@gmail.com>
2025-01-31 03:22:13 -0300

    Add loading spinner for posts

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -15,13 +15,16 @@ const PostList = () => {
   const [totalPost, setTotalPosts] = useState(0);
   const [selectedPostId, setSelectedPostId] = useState<number | null>(null);
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
   const postPerPage = 10;
 
   useEffect(() => {
     const fetchPosts = async () => {
+      setLoading(true);
       const postData = await getPosts(currentPage, postPerPage);
       setPosts(postData);
       setTotalPosts(100);
+      setLoading(false);
     };
 
     fetchPosts();
@@ -60,18 +63,25 @@ const PostList = () => {
   return (
     <>
       <Searcher onSearch={setSearchTerm} />
-
-      <div className="post-list">
-        {filteredPosts.map((post) => (
-          <PostCard
-            key={post.id}
-            id={post.id}
-            body={post.body}
-            title={post.title}
-            userId={post.userId}
-            onClick={() => handlePostClick(post.id)}
-          />
-        ))}
+      <div>
+        {loading ? (
+          <div className="loading-spinner">
+            <div className="spinner"></div>
+          </div>
+        ) : (
+          <div className="post-list">
+            {filteredPosts.map((post) => (
+              <PostCard
+                key={post.id}
+                id={post.id}
+                body={post.body}
+                title={post.title}
+                userId={post.userId}
+                onClick={() => handlePostClick(post.id)}
+              />
+            ))}
+          </div>
+        )}
       </div>
 
       {isModalOpen && selectedPostId !== null && (
